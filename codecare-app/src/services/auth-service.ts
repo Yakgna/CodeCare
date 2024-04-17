7fd9b04bf497779857f0f8efa5a4e23cacf14787
GokulakrishnanRavichandra <r.go@northeastern.edu>
2024-04-16 20:45:57 -0400

    feat: auth-service, axios interceptor

--- codecare-app/src/services/auth-service.ts
+++ codecare-app/src/services/auth-service.ts
@@ -1,7 +1,7 @@
 import {post} from "./api-service.ts";
 import {LoginDO} from "../models/LoginDO.ts";
 
-const loginApi = '/public/login';
+const loginApi = 'public/login';
 
 export const login = async (data: FormData):  Promise<LoginDO> => {
     return await post(loginApi, data);
