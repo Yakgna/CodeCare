3c038b4afc2763c29e301f1e3e48eec34b369764
Yakgna <ramasamy.y@northeastern.edu>
2024-04-16 20:38:02 -0400

    Added REST API implementation for Events module

--- codecare-service/app/routes/index.js
+++ codecare-service/app/routes/index.js
@@ -1,6 +1,7 @@
 import publicRoute from "./public-route.js";
 import auth from "../middleware/auth.js";
 import userRoute from "./user-route.js";
+import eventRoute from "./event-route.js";
 import {Roles} from "../entities/roles-enum.js";
 import authRoute from "./auth-route.js";
 
@@ -8,6 +9,7 @@ const initializeRoutes = (app) => {
     app.use('/api/public', publicRoute);
     app.use('/api/auth', authRoute);
     app.use('/api/user', auth([Roles.ADMIN]), userRoute);
+    app.use('/api/events', eventRoute);
 }
 
 export default initializeRoutes;
