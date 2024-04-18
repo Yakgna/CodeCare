fee705e78d3851595a642d74474e9a3b38adb620
Rohithvarma8 <datla.b@northeastern.edu>
2024-04-18 00:19:16 -0400

    feat:- used middleware for authentication and created reastApi for profile and medical-diagnosis

--- codecare-service/app/controller/profile-controller.js
+++ codecare-service/app/controller/profile-controller.js
@@ -1,62 +1,61 @@
 import { StatusCodes } from "http-status-codes";
 import * as ProfileService from '../services/profile-service.js';
-import { setErrorResponse, setSuccessResponse } from '../utils/response-handler.js';
+import { setErrorCode, setSuccessResponse } from '../utils/response-handler.js';
 
 
-export const save = async (req, res, next) => {
+export const save = async (req, res) => {
     try {
         
-        const userData = { ...req.body, userId: req.user};  // Include userId from authenticated user
+        const userData = { ...req.body, user: req.user._id};  // Include userId from authenticated user
         const profile = await ProfileService.save(userData);
         setSuccessResponse(StatusCodes.CREATED, profile, res);
 
     } catch (error) {
         console.log(error);
-        setErrorResponse(error, res);
-    }
+        setErrorCode(StatusCodes.INTERNAL_SERVER_ERROR, res);
+     }
 };
 
 export const searchByUserId = async (req, res) => {
-    try {
-        const userId = req.params.userId;  // Assuming userId is passed as a query parameter
+   try {
+        const userId = req.user._id;  // Assuming userId is passed as a query parameter
         const profile = await ProfileService.searchByUserId(userId);
         setSuccessResponse(StatusCodes.OK, profile, res);
     } catch (error) {
         console.log(error);
-        setErrorResponse(error, res);
+        setErrorCode(StatusCodes.INTERNAL_SERVER_ERROR, res);
     }
 };
 
 export const update = async (req, res) => {
     try {
-        const id = req.params.userId;  // Assuming id in params is the ObjectId of the user's profile
+        const id = req.user._id;  // Assuming id in params is the ObjectId of the user's profile
         const updatedProfile = await ProfileService.update(id, req.body);
         setSuccessResponse(StatusCodes.OK, updatedProfile, res);
     } catch (error) {
         console.log(error);
-        setErrorResponse(error.message, res, StatusCodes.INTERNAL_SERVER_ERROR);
+        setErrorCode(StatusCodes.INTERNAL_SERVER_ERROR, res);
     }
 };
 
 export const remove = async (req, res) => {
     try {
-        const id = req.params.userId;  // Assuming id in params is the ObjectId of the user's profile
+        const id = req.user._id;  // Assuming id in params is the ObjectId of the user's profile
         await ProfileService.remove(id);
         setSuccessResponse(StatusCodes.NO_CONTENT, {}, res);
     } catch (error) {
         console.log(error);
-        setErrorResponse(error.message, res, StatusCodes.INTERNAL_SERVER_ERROR);
+        setErrorCode(StatusCodes.INTERNAL_SERVER_ERROR, res);
     }
 };
 
 export const get = async (req, res) => {
     try {
-        const profile = await ProfileService.get(req.params.userId);
-        console.log(profile,req.params.userId)  // Assuming req.params.id is the ObjectId of the profile
+        const profile = await ProfileService.get(req.user._id); // Assuming req.params.id is the ObjectId of the profile
         setSuccessResponse(StatusCodes.OK, profile, res);
 
     } catch (error) {
         console.log(error);
-        setErrorResponse(error, res);
+        setErrorCode(StatusCodes.INTERNAL_SERVER_ERROR, res);
     }
 };
--- codecare-service/app/routes/profile-route.js
+++ codecare-service/app/routes/profile-route.js
@@ -1,14 +1,15 @@
 import express from "express";
 import * as ProfileController from "./../controller/profile-controller.js";
+import auth from "../middleware/auth.js";
+import { Roles } from "../entities/roles-enum.js";
 
 const router = express.Router();
 
-router.route('/:userId')
-    .get(ProfileController.get)
-    .put(ProfileController.update)
-    .delete(ProfileController.remove);
 router.route('/')
-    .post(ProfileController.save);
+    .get(auth([Roles.USER]),ProfileController.get)
+    .put(auth([Roles.USER]),ProfileController.update)
+    .delete(auth([Roles.ADMIN]),ProfileController.remove)
+    .post(auth([Roles.USER]),ProfileController.save);
 router.route('/expand/:userId')
     .get(ProfileController.searchByUserId);
 export default router;
