53be20deeab86a66dffbdb7ced587e181ae055e2
GokulakrishnanRavichandra <r.go@northeastern.edu>
2024-04-20 18:30:48 -0400

    feat: clerk initial setup

--- codecare-app/src/components/Homepage/Header.tsx
+++ codecare-app/src/components/Homepage/Header.tsx
@@ -2,81 +2,78 @@ import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import { Fade } from "react-awesome-reveal";
+import {Fade} from "react-awesome-reveal";
 
 //utilities
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import MyButton from '../../utils/MyButton';
+import {SignedIn, SignedOut, UserButton} from "@clerk/clerk-react";
 
 
 interface HeaderProps {
-  sections: ReadonlyArray<{
+    sections: ReadonlyArray<{
+        title: string;
+        url: string;
+    }>;
     title: string;
-    url: string;
-  }>;
-  title: string;
 }
 
 export default function Header(props: HeaderProps) {
-  const { sections, title } = props;
+    const {sections, title} = props;
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
 
+    return (
 
-  return (
-    <React.Fragment>
-<Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-  <Typography
-    component="h2"
-    variant="h5"
-    color="inherit"
-    align="center"
-    noWrap
-    sx={{ flex: 1 }}
-  >
-    <Fade delay={1000} triggerOnce>
-      {title}
-    </Fade>
-  </Typography>
-  <MyButton
-    label="Login"
-    onClick={() => navigate(`/signin`)}
-    variant="outlined"
-    sx={{ marginRight: 1 }} // Add margin to the right of the button
-  />
-  <MyButton
-    label="Sign Up"
-    onClick={() => navigate(`/signup`)}
-    variant="outlined"
-    sx={{ marginLeft: 1 }} // Add margin to the left of the button
-  />
-  <MyButton
-      label="Donate"
-      onClick={() => navigate(`/donate`)}
-      variant="outlined"
-      sx={{ marginLeft: 1 }} // Add margin to the left of the button
-  />
-</Toolbar>
-<Toolbar
-  component="nav"
-  variant="dense"
-  sx={{ overflowX: 'auto', gap: 2}}
->
-  {sections.map((section) => (
-    <Link
-      color="inherit"
-      noWrap
-      key={section.title}
-      variant="body2"
-      onClick={()=>{ return navigate(section.url)}}
-      sx={{ p: 1, flexShrink: 0, cursor: 'pointer' }}
-    >
-      {section.title}
-    </Link>
-  ))}
-</Toolbar>
+        <React.Fragment>
 
-    </React.Fragment>
-  );
+            <Toolbar sx={{borderBottom: 1, borderColor: 'divider'}}>
+                <Typography
+                    component="h2"
+                    variant="h5"
+                    color="inherit"
+                    align="center"
+                    noWrap
+                    sx={{flex: 1}}
+                >
+                    <Fade delay={1000} triggerOnce>
+                        {title}
+                    </Fade>
+                </Typography>
+                <SignedIn>
+                    <UserButton afterSignOutUrl='/signin'/>
+                </SignedIn>
+                <SignedOut>
+                    <Link href="/signin">Sign In</Link>
+                </SignedOut>
+                <MyButton
+                    label="Donate"
+                    onClick={() => navigate(`/donate`)}
+                    variant="outlined"
+                    sx={{marginLeft: 1}} // Add margin to the left of the button
+                />
+            </Toolbar>
+            <Toolbar
+                component="nav"
+                variant="dense"
+                sx={{overflowX: 'auto', gap: 2}}
+            >
+                {sections.map((section) => (
+                    <Link
+                        color="inherit"
+                        noWrap
+                        key={section.title}
+                        variant="body2"
+                        onClick={() => {
+                            return navigate(section.url)
+                        }}
+                        sx={{p: 1, flexShrink: 0, cursor: 'pointer'}}
+                    >
+                        {section.title}
+                    </Link>
+                ))}
+            </Toolbar>
+        </React.Fragment>
+    );
 }
--- codecare-app/src/main.tsx
+++ codecare-app/src/main.tsx
@@ -6,13 +6,22 @@ import {Provider} from "react-redux";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n.ts";
 import {store} from "./store";
+import {ClerkProvider} from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <I18nextProvider i18n={i18n}>
-                <RouterProvider router={router}/>
-            </I18nextProvider>
-        </Provider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <Provider store={store}>
+                <I18nextProvider i18n={i18n}>
+                    <RouterProvider router={router}/>
+                </I18nextProvider>
+            </Provider>
+        </ClerkProvider>
     </React.StrictMode>,
 )
