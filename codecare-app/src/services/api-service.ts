b435ae1ea5cbdb43144528d8b7ef9c1baeecb65b
GokulakrishnanRavichandra <r.go@northeastern.edu>
2024-04-21 06:54:34 -0400

    feat: axios api error handling

--- codecare-app/src/services/api-service.ts
+++ codecare-app/src/services/api-service.ts
@@ -1,6 +1,6 @@
 import axios from "./axios-config.ts";
 import {ResponseObject} from "../models/ResponseObject.ts";
-import {AxiosResponse} from "axios";
+import {AxiosError, AxiosResponse, isAxiosError} from "axios";
 
 const parseResponse = <T>(response: AxiosResponse<T>) => {
     const data: any = response.data;
@@ -9,44 +9,98 @@ const parseResponse = <T>(response: AxiosResponse<T>) => {
 }
 
 export const search = async <T>(path: string, params: any = {}): Promise<ResponseObject<T[]>> => {
+    let response: any = {};
     const query: URLSearchParams = new URLSearchParams(params);
-    const response = await axios.get(`/${path}?${query}`);
+    await axios.get(`/${path}?${query}`).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error;
+        } else {
+            throw error;
+        }
+    })
     return parseResponse(response);
 };
 
 export const getById = async <T>(path: string, id: string): Promise<ResponseObject<T>> => {
-    const response = await axios.get(`/${path}/${id}`);
+    let response: any = {};
+    await axios.get(`/${path}/${id}`).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error;
+        } else {
+            throw error;
+        }
+    });
     return parseResponse(response);
 };
 
 export const postForm = async <T>(path: string, object: any): Promise<ResponseObject<T>> => {
-    const response = await axios.post(`/${path}`, object, {
+    let response: any = {};
+    await axios.post(`/${path}`, object, {
         headers: {
             'Content-Type': 'application/json'
         }
-    });
+    }).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error.response;
+        } else {
+            throw error;
+        }
+    })
     return parseResponse(response);
 };
 
 export const save = async <T>(path: string, object: T): Promise<ResponseObject<any>> => {
-    const response = await axios.post(`/${path}`, JSON.stringify(object), {
+    let response: any = {};
+    await axios.post(`/${path}`, JSON.stringify(object), {
         headers: {
             'Content-Type': 'application/json'
         }
-    });
+    }).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error;
+        } else {
+            throw error;
+        }
+    })
     return parseResponse(response);
 };
 
 export const update = async <T>(path: string, object: T): Promise<ResponseObject<any>> => {
-    const response = await axios.put(`/${path}/${object.id}`, JSON.stringify(object), {
+    let response: any = {};
+    await axios.put(`/${path}/${object.id}`, JSON.stringify(object), {
         headers: {
             'Content-Type': 'application/json',
         }
-    });
+    }).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error;
+        } else {
+            throw error;
+        }
+    })
     return parseResponse(response);
 };
 
 export const deleteById = async (path: string, id: string): Promise<ResponseObject<any>> => {
-    const response = await axios.delete(`/${path}/${id}`);
+    let response: any = {};
+    await axios.delete(`/${path}/${id}`).then((axiosResponse: AxiosResponse) => {
+        response = axiosResponse;
+    }).catch((error: Error | AxiosError) => {
+        if (isAxiosError(error))  {
+            response = error;
+        } else {
+            throw error;
+        }
+    })
     return parseResponse(response);
 };
